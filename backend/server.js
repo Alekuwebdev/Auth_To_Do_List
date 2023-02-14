4b9c237a659e760b6382eae64d34e813fe9e6d0f
Alecu <alecugajos1@gmail.com>
2023-02-14 14:41:48 +0100

    commit

--- backend/server.js
+++ backend/server.js
@@ -4,9 +4,16 @@ const express = require('express')
 const mongoose = require('mongoose')
 const todoRoutes = require('./routes/todos')
 const userRoutes = require('./routes/user')
+const cors = require('cors')
 
 // express app
 const app = express()
+
+// cors
+app.use(cors({
+    // origin: 'http://localhost:3000'
+    origin: 'https://bespoke-unicorn-c72030.netlify.app'
+}))
 
 // middleware
 app.use(express.json())
